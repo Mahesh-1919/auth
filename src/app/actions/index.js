86841ef3748d77e97ed4e9be39b02ba9f25d0e81
Mahesh-1919 <maheshjyothula23@gmail.com>
2024-09-24 10:56:26 +0530

    fetch

--- src/app/actions/index.js
+++ src/app/actions/index.js
@@ -35,7 +35,7 @@ export async function doRegister(formData) {
   }
   const token = generateToken();
   try {
-    const response = await fetch("http://localhost:3000/api/register", {
+    const response = await fetch("http://auth.smdev.me/api/register", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
